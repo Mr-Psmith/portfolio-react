805970504ab1b8b6579454e781c491983f80e448
Mr-Psmith <psancthus@gmail.com>
2023-08-15 10:35:08 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         work in progress
+        </p>
+        <a href='https://psancthus.com/' className="App-link">
+          old portfolio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
